5152f64f6bc2b350c37467d79441206e92eb3b4a
Justin Gardner <justin@rhynorater.com>
2024-03-28 06:02:55 -0500

    5.54

--- malcare-security/malcare.php
+++ malcare-security/malcare.php
@@ -5,7 +5,7 @@ Plugin URI: https://www.malcare.com
 Description: MalCare WordPress Security Plugin - Malware Scanner, Cleaner, Security Firewall
 Author: MalCare Security
 Author URI: https://www.malcare.com
-Version: 5.53
+Version: 5.54
 Network: True
  */
 
